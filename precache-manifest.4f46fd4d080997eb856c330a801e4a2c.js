4c8e2e4e5e3fdf8d04966df867f4db27a5590420
vartika06 <vartika.1998@gmail.com>
2019-07-09 18:06:36 +0530

    Updates

---
+++ precache-manifest.4f46fd4d080997eb856c330a801e4a2c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "384cfeeebc1f393a6e45487575569252",
+    "url": "/PORTFOLIO/index.html"
+  },
+  {
+    "revision": "7eb582cecd2dd68333bf",
+    "url": "/PORTFOLIO/static/js/2.c26854d5.chunk.js"
+  },
+  {
+    "revision": "c7c13df00f7a709b2e0a",
+    "url": "/PORTFOLIO/static/js/main.21e28a18.chunk.js"
+  },
+  {
+    "revision": "c90b415f342b24257720",
+    "url": "/PORTFOLIO/static/js/runtime~main.bba35dba.js"
+  }
+]);
